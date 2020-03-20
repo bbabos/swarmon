e0d4186fd5d201c30de170c861e4ee735cb5c8c1
Babós Bence <bence.babos@calmarkr.com>
2020-03-20 19:24:48 +0100

    add docker page

--- pages.go
+++ pages.go
@@ -20,9 +20,40 @@ func menuPage() {
 			stackInit()
 		case "2":
 			validInput = false
-			fmt.Println("TODO")
+			dockerPage()
 		case "3":
 			validInput = false
+		default:
+			fmt.Printf("%s is not a valid option\n", choosen)
+		}
+	}
+}
+
+func dockerPage() {
+	fmt.Println("1. List all running container IDs")
+	fmt.Println("2. List services")
+	fmt.Println("3. List swarm nodes")
+	fmt.Println("4. Exit to main menu")
+
+	validInput := true
+
+	for validInput {
+		fmt.Print("Choose an option: ")
+		choosen := readInput()
+
+		switch choosen {
+		case "1":
+			listContainerIDs()
+			fmt.Println()
+		case "2":
+			listServices()
+			fmt.Println()
+		case "3":
+			listSwarmNodes()
+			fmt.Println()
+		case "4":
+			validInput = false
+			menuPage()
 		default:
 			fmt.Printf("%s is not a valid option\n", choosen)
 		}
