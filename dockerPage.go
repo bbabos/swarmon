8441dc54933644233a3c7d1c4dab4277ea634b6b
Babós Bence <bence.babos@calmarkr.com>
2020-03-27 09:59:56 +0100

    rename page files

---
+++ dockerPage.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func containerOptions() {
+	var selected string
+	exit := true
+
+	clear()
+	fmt.Println(border)
+	fmt.Println("CONTAINERS")
+	fmt.Println(border)
+	listContainers()
+
+	for exit {
+		fmt.Println(border)
+		fmt.Println("0 - Back | 1 - Enter into a container")
+		fmt.Println(border)
+		fmt.Print("Select an option: ")
+		selected = readInput()
+
+		switch selected {
+		case "1":
+			// TODO
+		case "0":
+			exit = false
+			dockerPage()
+		default:
+			fmt.Printf("%s is not a valid option\n", selected)
+		}
+
+	}
+}
