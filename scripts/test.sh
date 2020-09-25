a41ee8b3c146313f03a096c592dc818a46015394
Bence Babós <bence.babos@tappointment.com>
2020-09-25 17:58:00 +0200

    minor polish

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-domain=$(< ../stackconfig.json jq -r '.Domain')
-schema=$(< ../stackconfig.json jq -r '.Schema')
-traefik_port=$(< ../stackconfig.json jq -r '.Traefik.Port')
-grafana_sub=$(< ../stackconfig.json jq -r '.Traefik.GrafanaSubDomain')
-alertm_sub=$(< ../stackconfig.json jq -r '.Traefik.AlertmanagerSubDomain')
-prom_sub=$(< ../stackconfig.json jq -r '.Traefik.PrometheusSubDomain')
-ba_user=$(< ../stackconfig.json jq -r '.Traefik.BAUser')
-ba_pass=$(< ../stackconfig.json jq -r '.Traefik.BAPassword')
+domain=$(< stackconfig.json jq -r '.Domain')
+schema=$(< stackconfig.json jq -r '.Schema')
+traefik_port=$(< stackconfig.json jq -r '.Traefik.Port')
+grafana_sub=$(< stackconfig.json jq -r '.Traefik.GrafanaSubDomain')
+alertm_sub=$(< stackconfig.json jq -r '.Traefik.AlertmanagerSubDomain')
+prom_sub=$(< stackconfig.json jq -r '.Traefik.PrometheusSubDomain')
+ba_user=$(< stackconfig.json jq -r '.Traefik.BAUser')
+ba_pass=$(< stackconfig.json jq -r '.Traefik.BAPassword')
+slack_webhook=$(< stackconfig.json jq -r '.Slack.Webhook')
+slack_user=$(< stackconfig.json jq -r '.Slack.AlertUser')
+slack_channel=$(< stackconfig.json jq -r '.Slack.Channel')
 grafana_domain=$schema://$grafana_sub.$domain:$traefik_port
 alertm_domain=$schema://$alertm_sub.$domain:$traefik_port
 prom_domain=$schema://$prom_sub.$domain:$traefik_port
-slack_webhook=$(< ../stackconfig.json jq -r '.Slack.Webhook')
-slack_user=$(< ../stackconfig.json jq -r '.Slack.AlertUser')
-slack_channel=$(< ../stackconfig.json jq -r '.Slack.Channel')
 
 function testSiteAccess {
     if [[ "$2" == BA ]]; then
@@ -22,16 +22,16 @@ function testSiteAccess {
         curl -H Host:$1.$domain $schema://$domain:$traefik_port > /dev/null 2>&1
     fi
     if [[ "$?" != 0 ]]; then
-        echo "TEST FAILED > testSiteAccess with subdomain: $1"
+        echo "TEST FAILED  > testSiteAccess with subdomain: $1"
     else
         echo "TEST SUCCEED > testSiteAccess with subdomain: $1"
     fi
 }
 
 function testSlackIntegration {
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"SwarMon integration test","channel":"'$slack_channel'","username":"'"$slack_user"'"}' $slack_webhook > /dev/null 2>&1
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"swarmon integration test","channel":"'$slack_channel'","username":"'"$slack_user"'"}' $slack_webhook > /dev/null 2>&1
     if [[ "$?" != 0 ]]; then
-        echo "TEST FAILED > testSlackIntegration"
+        echo "TEST FAILED  > testSlackIntegration"
     else
         echo "TEST SUCCEED > testSlackIntegration"
     fi
@@ -47,7 +47,7 @@ function testDockerServices {
             if [[ "$?" != 0 ]]; then
                 echo "TEST SUCCEED > testDockerServices on service: $servicename"
             else
-                echo "TEST FAILED > testDockerServices on service: $servicename"
+                echo "TEST FAILED  > testDockerServices on service: $servicename"
             fi
         fi
     done
