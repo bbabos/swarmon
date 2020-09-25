0f5c393795e7ce38d01ef5f99315df5fcb845fef
Bence Babós <bence.babos@tappointment.com>
2020-09-25 09:40:19 +0200

    fix alertmanager entrypoint script

--- configs/docker/alertmanager/conf/entrypoint.sh
+++ configs/docker/alertmanager/conf/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 cd /etc/alertmanager
-< alertmanager.yml "s@#url#@api_url: $SLACK_URL@g" |
+< alertmanager.yml sed "s@#url#@api_url: $SLACK_URL@g" |
     sed "s@#user#@username: $SLACK_USER@g" |
     sed "s@#title_link#@title_link: $SCHEMA://alerts.$DOMAIN@g" >tmp_alertmanager.yml
 mv tmp_alertmanager.yml alertmanager.yml
